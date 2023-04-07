7ddd695ccbcdf46239924984dcec7df1d136fe4c
tkylesh <taylorshuff90@gmail.com>
2023-04-07 14:32:41 -0500

    added some more stuff to program.cs.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@
                     Create at least one instance of each kind of baked good
              */
 
+                Macaroon yellowMacaroon = new Macaroon();
+                Pie ReesesPie = new Pie();
 
 
 
@@ -24,6 +26,16 @@
                 Step 3:
                     Create a List<> and add all baked goods to the list
              */
+
+             Macaroon pinkMacaroon = new Macaroon();
+             Pie PecanPie = new Pie();
+             Macaroon greenMacaroon = new Macaroon();
+             Pie ChocolateMoussePie = new Pie();
+
+
+             Licorice twisters = new Licorice();
+             Sours warheads = new Sours();
+
 
         }
     }
